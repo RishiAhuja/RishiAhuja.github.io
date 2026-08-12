e5942621bced4b2e9ee6155b4b4f55ab57a168f7
RishiAhuja <www.rishiahuja@gmail.com>
2026-08-12 20:31:16 +0530

    Keep CV and profile URLs in one place.

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const PORTRAIT = '/images/rishi-ahuja.jpg';
+
+export const LINKS = {
+  RESUME: 'https://rishia.in/research/resume',
+  TWITTER: 'https://twitter.com/Rishi2220',
+  GITHUB: 'https://github.com/RishiAhuja',
+  LINKEDIN: 'https://www.linkedin.com/in/rishi-ahuja-b1a224310',
+  GOOGLE_SCHOLAR: 'https://scholar.google.com/citations?hl=en&user=EZff3KsAAAAJ',
+  OPENREVIEW: 'https://openreview.net/profile?id=~Rishi_Ahuja1',
+  EMAIL: 'mailto:rishia2220@gmail.com',
+} as const;
